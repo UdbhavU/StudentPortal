69624b7337fc70265298e34002e9935b57aeef43
UdbhavU <udbhav.ud@outlook.com>
2020-10-17 15:39:54 +0530

    added routing to home

--- forum/views.py
+++ forum/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-#following are just dummy contents
-posts = {
-    'title':'title 1',
-    'author':'usr 1',
-    'content':'hello world',
-    'date_created':'Oct-20-2020'
-}
+#following are list just dummy contents
+#when accessing from databse make sure that the content is a list 
+##todo Access the database content and replace posts with the content from the database 
+posts = [{
+     'author':'Percy',
+     'title':'welcome',
+     'content':'first post',
+     'date_posted':'October 15, 2020'
+    },
+    {
+     'author':'Andromeda',
+     'title':'welcome again',
+     'content':'Second post',
+     'date_posted':'October 16, 2020'
+    },
+    ]
 #Index/Home page page view
 def home(request):
-    return render(request,'forum/base.html')
+    return render(request,'forum/home.html')
 
 #forum app view
 def forum(request):
