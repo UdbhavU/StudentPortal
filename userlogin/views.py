10935da751d1bb8c0a7814038d84e2a431612d0d
UdbhavU <udbhav.ud@outlook.com>
2020-11-02 10:17:46 +0530

    Added Working Login System

--- userlogin/views.py
+++ userlogin/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def userlogin(request):
-    pass
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username,password)
+        user = authenticate(request,username=username,password=password)
+        print(user)
+        if user is not None:
+            return render(request,'forum/forum.html')
+        else:
+             context = {
+            'error':'Invalid Username or Password '
+        }
+        return render(request,'userlogin/signin.html',context)
+            
+    else:
+       return render(request,'userlogin/signin.html')
+
+
