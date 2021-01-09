9d98b2f9f6959db769123c5e277e5e70c27104a1
UdbhavU <udbhav.ud@outlook.com>
2021-01-09 13:05:35 +0530

    add bgp

--- StudentPortal/settings.py
+++ StudentPortal/settings.py
@@ -92,11 +92,28 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/root/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "root")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR,"")
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "/static/")]
+#
+STATIC_ROOT = os.path.join(BASE_DIR, '')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'asset'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 MEDIA_URL="/media/"
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
--- StudentPortal/urls.py
+++ StudentPortal/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.contrib import admin
 
@@ -15,4 +15,5 @@ urlpatterns = [
                   path('', include('profiles.urls')),
                   path('', include('timetable.urls')),
 
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
